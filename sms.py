6f3f575c6f228840a8a318ab8538e19512de8a0c
itshella-dom <itshella-dom@users.noreply.github.com>
2014-09-03 10:54:10 -0700

    Update sms.py
    Removed auth token and sid.

--- sms.py
+++ sms.py
@@ -3,8 +3,8 @@ from twilio.rest import TwilioRestClient
  
 # Find these values at https://twilio.com/user/account
 # Need to change these credentials 
-account_sid ="ACacc3cd1f26418f78dd415ef0454924be"
-auth_token = "bd47541aec30109464db256def130777"
+account_sid ="NEED TO ADD"
+auth_token = "NEED TO ADD"
 client = TwilioRestClient(account_sid, auth_token)
  
 message = client.messages.create(to="+19252197076", from_="+14157128352",
